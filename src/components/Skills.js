073c8dbefaca2038fb4573e639dfdf708c395cd9
Ryan Howard <ryanghoward@protonmail.com>
2022-07-11 14:05:50 -0700

    Added even more skillz

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,6 +13,16 @@ import PostgreSQL from "../assets/images/skills/PostgreSQL.png";
 import Heroku from "../assets/images/skills/heroku.svg";
 import Vercel from "../assets/images/skills/vercel.svg";
 import Bootstrap from "../assets/images/skills/bootstrap.png";
+import Next from "../assets/images/skills/next.png";
+import MUI from "../assets/images/skills/mui.png";
+import Less from "../assets/images/skills/less.webp";
+import Sass from "../assets/images/skills/sass.png";
+import SC from "../assets/images/skills/styledcomponents.png";
+import SQLite from "../assets/images/skills/SQLite.png";
+import Docker from "../assets/images/skills/docker.webp";
+import ESLint from "../assets/images/skills/ESLint.png";
+import Postman from "../assets/images/skills/postman.png";
+// import Trello from "../assets/images/skills/trello.png";
 import colorSharp from "../assets/images/color-sharp.png";
 
 export const Skills = () => {
@@ -97,6 +107,46 @@ export const Skills = () => {
                   <img src={Bootstrap} alt='' />
                   <h5>Bootstrap</h5>
                 </div>
+                <div className='item'>
+                  <img src={Next} alt='' />
+                  <h5>Next.js</h5>
+                </div>
+                <div className='item'>
+                  <img src={MUI} alt='' />
+                  <h5>MUI</h5>
+                </div>
+                <div className='item'>
+                  <img src={Less} alt='' />
+                  <h5>Less</h5>
+                </div>
+                <div className='item'>
+                  <img src={Sass} alt='' />
+                  <h5>Sass</h5>
+                </div>
+                <div className='item'>
+                  <img src={SC} alt='' />
+                  <h5>Styled Components</h5>
+                </div>
+                <div className='item'>
+                  <img src={SQLite} alt='' />
+                  <h5>SQLite</h5>
+                </div>
+                <div className='item'>
+                  <img src={Docker} alt='' />
+                  <h5>Docker</h5>
+                </div>
+                <div className='item'>
+                  <img src={ESLint} alt='' />
+                  <h5>ESLint</h5>
+                </div>
+                <div className='item'>
+                  <img src={Postman} alt='' />
+                  <h5>Postman</h5>
+                </div>
+                {/* <div className='item'>
+                  <img src={Trello} alt='' />
+                  <h5>Trello</h5>
+                </div> */}
               </Carousel>
             </div>
           </Col>
