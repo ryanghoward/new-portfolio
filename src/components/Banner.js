9cf51d0b78a265704487fcff0072f83f8ed4716f
Ryan Howard <ryanghoward@protonmail.com>
2022-07-11 15:28:49 -0700

    react-typist sucked ass, so I got rid of it and switched to react-type-animation. Much Better.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,54 +1,11 @@
-import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import Astronaut from "../assets/images/astronaut.svg";
-import guycoding from "../assets/images/guycoding.png";
-import "animate.css";
-import TrackVisibilty from "react-on-screen";
-import Typist from "react-typist";
+// import Astronaut from "../assets/images/astronaut.svg";
+// import guycoding from "../assets/images/guycoding.png";
+// import "animate.css";
+import TypeAnimation from "react-type-animation";
 
 export const Banner = () => {
-  // const [loopNum, setLoopNum] = useState(0);
-  // const [isDeleting, setIsDeleting] = useState(false);
-  // const [text, setText] = useState("");
-  // const [delta, setDelta] = useState(300 - Math.random() * 100);
-  // const toRotate = [
-  //   "Fullstack Web Developer",
-  //   "Web Designer",
-  //   "UI/UX Designer",
-  // ];
-  // const period = 500;
-
-  // useEffect(() => {
-  //   let ticker = setInterval(() => {
-  //     tick();
-  //   }, delta);
-  //   return () => {
-  //     clearInterval(ticker);
-  //   };
-  // }, [text]);
-
-  // const tick = () => {
-  //   let i = loopNum % toRotate.length;
-  //   let fullText = toRotate[i];
-  //   let updatedText = isDeleting
-  //     ? fullText.substring(0, text.length - 1)
-  //     : fullText.substring(0, text.length + 1);
-  //   setText(updatedText);
-  //   if (isDeleting) {
-  //     setDelta((prevDelta) => prevDelta / 2);
-  //   }
-
-  //   if (!isDeleting && updatedText === fullText) {
-  //     setIsDeleting(true);
-  //     setDelta(period);
-  //   } else if (isDeleting && updatedText === "") {
-  //     setIsDeleting(false);
-  //     setLoopNum(loopNum + 1);
-  //     setDelta(500);
-  //   }
-  // };
-
   return (
     <section className='banner' id='home'>
       <Container>
@@ -57,15 +14,19 @@ export const Banner = () => {
             <span className='tagline'>
               Hello! I'm Ryan, welcome to my portfolio!
             </span>
-            <h1>
-              <Typist>
-                <span>Phrase 1</span>
-                <Typist.Backspace count={17} delay={200} />
-                <span>Phrase 2</span>
-              </Typist>
-              {/* I'm Ryan, a <br />
-              <span className='wrap'>{text}</span> */}
-            </h1>
+            <TypeAnimation
+              cursor={true}
+              repeat={Infinity}
+              sequence={[
+                "Fullstack Developer",
+                1000,
+                "UI/UX Designer",
+                1000,
+                "Web Developer",
+                1000,
+              ]}
+              wrapper='h1'
+            />
             <p>
               I am passionate about building excellent software that improves
               the lives of those around me. I specialize in creating software
