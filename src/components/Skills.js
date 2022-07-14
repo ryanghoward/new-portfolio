952c9eccf9a63311edbf39b2193abc2d662a0d15
Ryan Howard <ryanghoward@protonmail.com>
2022-07-13 17:47:39 -0700

    save

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,12 +5,13 @@ import JavaScript from "../assets/images/skills/javascript.png";
 import HTML from "../assets/images/skills/html.png";
 import CSS from "../assets/images/skills/css.png";
 import Node from "../assets/images/skills/node.png";
+import GitHub from "../assets/images/skills/github.png";
 // import Express from "../assets/images/skills/express.png";
 import Cypress from "../assets/images/skills/cypress.svg";
+import Jest from "../assets/images/skills/jest.png";
 import React from "../assets/images/skills/react.png";
 import Redux from "../assets/images/skills/redux.png";
 import Tailwind from "../assets/images/skills/tailwind.png";
-import PostgreSQL from "../assets/images/skills/PostgreSQL.png";
 import Heroku from "../assets/images/skills/heroku.svg";
 import Vercel from "../assets/images/skills/vercel.svg";
 import Bootstrap from "../assets/images/skills/bootstrap.png";
@@ -20,6 +21,8 @@ import Less from "../assets/images/skills/less.webp";
 import Sass from "../assets/images/skills/sass.png";
 import SC from "../assets/images/skills/styledcomponents.png";
 import SQLite from "../assets/images/skills/SQLite.png";
+import PostgreSQL from "../assets/images/skills/PostgreSQL.png";
+import MySQL from "../assets/images/skills/mysql.png";
 import Docker from "../assets/images/skills/docker.webp";
 import ESLint from "../assets/images/skills/ESLint.png";
 import Postman from "../assets/images/skills/postman.png";
@@ -57,11 +60,15 @@ export const Skills = () => {
                 responsive={responsive}
                 infinite={true}
                 className='skill-slider'
-                autoPlay={true}
+                // autoPlay={true}
                 autoPlaySpeed={1000}
                 showArrows={false}
                 removeArrowOnDeviceType={["tablet", "mobile"]}
               >
+                <div className='item'>
+                  <img src={JavaScript} alt='' />
+                  <h5>JavaScript</h5>
+                </div>
                 <div className='item'>
                   <img src={HTML} alt='' />
                   <h5>HTML</h5>
@@ -71,16 +78,20 @@ export const Skills = () => {
                   <h5>CSS</h5>
                 </div>
                 <div className='item'>
-                  <img src={JavaScript} alt='' />
-                  <h5>JavaScript</h5>
-                </div>
-                <div className='item'>
                   <img src={Node} alt='' />
                   <h5>Node.js</h5>
                 </div>
                 <div className='item'>
+                  <img src={GitHub} alt='' />
+                  <h5>GitHub</h5>
+                </div>
+                <div className='item'>
                   <img src={Cypress} alt='' />
                   <h5>Cypress</h5>
+                </div>
+                <div className='item'>
+                  <img src={Jest} alt='' />
+                  <h5>Jest</h5>
                 </div>
                 {/* <div className='item'>
                   <img src={Express} alt='' />
@@ -97,10 +108,6 @@ export const Skills = () => {
                 <div className='item'>
                   <img src={Tailwind} alt='' />
                   <h5>Tailwind</h5>
-                </div>
-                <div className='item'>
-                  <img src={PostgreSQL} alt='' />
-                  <h5>PostgreSQL</h5>
                 </div>
                 <div className='item'>
                   <img src={Heroku} alt='' />
@@ -137,6 +144,14 @@ export const Skills = () => {
                 <div className='item'>
                   <img src={SQLite} alt='' />
                   <h5>SQLite</h5>
+                </div>
+                <div className='item'>
+                  <img src={PostgreSQL} alt='' />
+                  <h5>PostgreSQL</h5>
+                </div>
+                <div className='item'>
+                  <img src={MySQL} alt='' />
+                  <h5>MySQL</h5>
                 </div>
                 <div className='item'>
                   <img src={Docker} alt='' />
