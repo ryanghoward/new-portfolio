4e10e010d759535ead9c77042bf97d4b19ad6546
Ryan Howard <ryanghoward@protonmail.com>
2022-08-03 14:17:00 -0700

    Added react-textra

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,11 @@ import "../CSS Files/Banner.css";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import me from "../assets/images/main/me.jpg";
-import TypeAnimation from "react-type-animation";
 import { Link } from "react-scroll";
 
+/* Extra Packages */
 import { GlitchText } from "glitch-text";
+import Textra from "react-textra";
 
 export const Banner = () => {
   return (
@@ -18,31 +19,19 @@ export const Banner = () => {
               <h1>
                 <GlitchText theme='purple' text={"Ryan G. Howard"} />
               </h1>
-              {/* <h1>Ryan G. Howard</h1> */}
               <h3>I am a</h3>
-              <TypeAnimation
-                wrapper='h2'
-                cursor={true}
-                repeat={Infinity}
-                sequence={[
-                  "Web Developer",
-                  2000,
-                  "",
-                  1000,
-                  "UI/UX Designer",
-                  2000,
-                  "",
-                  1000,
-                  "Full Stack Developer",
-                  1000,
-                  "",
-                  1000,
-                  "Techie",
-                  1000,
-                  "",
-                  1000,
-                ]}
-              />
+              <h2>
+                <Textra
+                  effect='downTop'
+                  stopDuration={1500}
+                  data={[
+                    "Web Developer",
+                    "UI/UX Designer",
+                    "Full Stack Developer",
+                    "Techie",
+                  ]}
+                />
+              </h2>
               <p>
                 I am passionate about building excellent software that improves
                 the lives of those around me. I specialize in creating software
